3a4aa9d0e1fe9f53bcc7673406474bef096fb4ed
niritsiq <154353994+niritsiq@users.noreply.github.com>
2024-01-07 22:56:50 +0200

    Add files via upload

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,23 +1,23 @@
 /**
-* Computes the periodical payment necessary to re-pay a given loan.
-*/
+ * Computes the periodical payment necessary to re-pay a given loan.
+ */
 public class LoanCalc {
-	
-	static double epsilon = 0.001;  // The computation tolerance (estimation error)
-	static int iterationCounter;    // Monitors the efficiency of the calculation
-	
-    /** 
-     * Gets the loan data and computes the periodical payment.
-     * Expects to get three command-line arguments: sum of the loan (double),
-     * interest rate (double, as a percentage), and number of payments (int).  
-     */
-	public static void main(String[] args) {		
+
+	static double epsilon = 0.001; // The computation tolerance (estimation error)
+	static int iterationCounter; // Monitors the efficiency of the calculation
+
+	/**
+	 * Gets the loan data and computes the periodical payment.
+	 * Expects to get three command-line arguments: sum of the loan (double),
+	 * interest rate (double, as a percentage), and number of payments (int).
+	 */
+	public static void main(String[] args) {
 		// Gets the loan data
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
-		
+
 		// Computes the periodical payment using brute force search
 		System.out.print("Periodical payment, using brute force: ");
 		System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
@@ -30,37 +30,69 @@ public class LoanCalc {
 		System.out.println();
 		System.out.println("number of iterations: " + iterationCounter);
 	}
-	
+
 	/**
-	* Uses a sequential search method  ("brute force") to compute an approximation
-	* of the periodical payment that will bring the ending balance of a loan close to 0.
-	* Given: the sum of the loan, the periodical interest rate (as a percentage),
-	* the number of periods (n), and epsilon, a tolerance level.
-	*/
+	 * Uses a sequential search method ("brute force") to compute an approximation
+	 * of the periodical payment that will bring the ending balance of a loan close
+	 * to 0.
+	 * Given: the sum of the loan, the periodical interest rate (as a percentage),
+	 * the number of periods (n), and epsilon, a tolerance level.
+	 */
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
-    }
-    
-    /**
-	* Uses bisection search to compute an approximation of the periodical payment 
-	* that will bring the ending balance of a loan close to 0.
-	* Given: the sum of theloan, the periodical interest rate (as a percentage),
-	* the number of periods (n), and epsilon, a tolerance level.
-	*/
-	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
-    }
-	
+	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+		// Replace the following statement with your code
+		double guess = loan / n;
+
+		while (endBalance(loan, rate, n, guess) > 0) {
+			guess += epsilon;
+			iterationCounter++;
+		}
+
+		return guess;
+	}
+
 	/**
-	* Computes the ending balance of a loan, given the sum of the loan, the periodical
-	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	*/
+	 * Uses bisection search to compute an approximation of the periodical payment
+	 * that will bring the ending balance of a loan close to 0.
+	 * Given: the sum of theloan, the periodical interest rate (as a percentage),
+	 * the number of periods (n), and epsilon, a tolerance level.
+	 */
+	// Side effect: modifies the class variable iterationCounter.
+	public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+		// Replace the following statement with your code
+		double lo = 0;
+		double hi = loan / n;
+		double guess;
+		iterationCounter = 0; // Reset iteration counter
+
+		while ((hi - lo) > epsilon) {
+			guess = (lo + hi) / 2;
+
+			if (endBalance(loan, rate, n, guess) * endBalance(loan, rate, n, lo) > 0)
+				lo = guess;
+			else
+				hi = guess;
+
+			iterationCounter++;
+		}
+
+		return (lo + hi) / 2;
+
+	}
+
+	/**
+	 * Computes the ending balance of a loan, given the sum of the loan, the
+	 * periodical
+	 * interest rate (as a percentage), the number of periods (n), and the
+	 * periodical payment.
+	 */
 	private static double endBalance(double loan, double rate, int n, double payment) {
 		// Replace the following statement with your code
-    	return 0;
+		double balance = loan;
+		for (int i = 1; i <= n; i++) {
+			balance = (balance - payment) * (1 + rate);
+		}
+		return balance;
+
 	}
 }
--- LowerCase.java
+++ LowerCase.java
@@ -1,17 +1,40 @@
 /** String processing exercise 1. */
 public class LowerCase {
-    public static void main(String[] args) {  
+    public static void main(String[] args) {
         String str = args[0];
         System.out.println(lowerCase(str));
     }
 
-   /**
-    * Returns a string which is identical to the original string, 
-    * except that all the upper-case letters are converted to lower-case letters.
-    * Non-letter characters are left as is.
-    */
+    /**
+     * Returns a string which is identical to the original string,
+     * except that all the upper-case letters are converted to lower-case letters.
+     * Non-letter characters are left as is.
+     */
     public static String lowerCase(String s) {
         // Replace the following statement with your code
-        return null;
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String update = "";
+        boolean check = false;
+        int count;
+        for (int i = 0; i < s.length(); i++) {
+            check = false;
+            count = -1;
+            for (int c = 0; c < upper.length(); c++) {
+                if (s.charAt(i) == upper.charAt(c)) {
+                    check = true;
+                    count = c;
+                }
+            }
+            if (check) {
+
+                if (s.charAt(i) == upper.charAt(count))
+                    update += lower.charAt(count);
+                else
+                    update += s.charAt(i);
+            } else
+                update += s.charAt(i);
+        }
+        return update;
     }
 }
